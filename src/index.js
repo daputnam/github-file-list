d28d0c1a63d7b409fb88e6a07c2beec2283f1bb7
Dan Putnam <daniel.a.putnam@gmail.com>
2018-04-19 23:17:36 -0500

    not working

--- src/index.js
+++ src/index.js
@@ -3,10 +3,35 @@ import ReactDOM from "react-dom";
 import PropTypes from 'prop-types'
 import "./index.css";
 
-const FileList = (props) => {
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
   return (
-    <div>file list</div>
+    <span className="time">{timeString}</span>
+  )
+}
+Time.propTypes = {
+  time: PropTypes.string
+}
+
+const FileListItem = (props) => {
+  return (
+    <div>File list item</div>
   )
 }
 
+const FileList = (props) => {
+  return (
+    <div>file list
+      {props.files.map(file) => {
+        return (
+          <FileListItem file={file}/>
+        )
+      }}
+      
+    </div>
+  )
+}
+FileList.PropTypes = {
+  files: PropTypes.array
+}
 ReactDOM.render(<FileList/>, document.getElementById("root"));
